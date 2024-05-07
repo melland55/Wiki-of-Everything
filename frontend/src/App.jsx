29a74ad30a1a4e07948d9ae61033e6768ef35cc0
melland55 <andrewmelland@gmail.com>
2024-05-07 03:01:44 -0500

    Added # scrolling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import AccordionItem from './AccordionItem';
 import SearchBar from './SearchBar';
@@ -8,12 +8,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 
-function App() {
-  const [summary, setSummary] = useState('');
-  const [sections, setSections] = useState([]);
-  const [topics, setTopics] = useState([]);
 
-  const { topic } = useParams();
+function App() {
+  const [summary, setSummary] = useState(''); //For Topic summary
+  const [sections, setSections] = useState([]); //For section titles and contents
+  const [topics, setTopics] = useState([]); //For Search bar results
+  const { topic } = useParams(); //For Topic of current page
+  const sectionRefs = useRef([]); //Ref to scroll to sections
+
+  const scrollToItem = (index) => {
+    if (sectionRefs.current[index]) {
+      sectionRefs.current[index].scrollIntoView({ behavior: 'smooth'});
+    }
+  };
+
+  useEffect(() => {
+    sectionRefs.current = Array(sections.length).fill(null).map((_, index) => sectionRefs.current[index]);
+  }, [sections]);
   
   useEffect(() => {
     const fetchData = async () => {
@@ -24,26 +35,41 @@ function App() {
         const summary_responseData = summary_response.data.response;
         setSummary(summary_responseData.summary);
         setSections(summary_responseData.sections);
+
       } catch (error) {
         console.error('Error fetching API data:', error);
       }
     };
-
     fetchData();
-
     return () => {
-
     };
   }, [topic]);
 
-  
+  useEffect(() => {
+    const handleHashChange = () => {
+      const hash = window.location.hash;
+      const hashtag = hash.substring(1).replace(/%20/g, ' '); // Remove '#' from the hash
+      let index = -1;
+      sections.forEach((section, idx) => {
+        if (section.title.toLowerCase() === hashtag.toLowerCase()) {
+          index = idx;
+          return;
+        }
+      });
+      scrollToItem(index)
+    };
+    window.addEventListener('hashchange', handleHashChange);
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange);
+    };
+  }, [sections]);
 
   return (
     <div className="wiki-page">
       <header className="custom-header">
         <div className="logo-container">
           <a href="/">
-            <img src="/Wiki-Logo.svg" alt="Your Logo" style={{ width: '64px', height: 'auto' }}/>
+            <img src="/logo.svg" alt="Your Logo" style={{ width: '64px', height: 'auto' }}/>
           </a>
         </div>
         <div className="navigation-container">
@@ -52,27 +78,35 @@ function App() {
       </header>
       <div className="content">
         <div className="sidebar">
-          <h2>Contents</h2>
-          {sections.length > 0 ? (
-            <ul>
-              {sections.map((section, index) => (
-                <li key={index}>
-                  <a href={`#${section.title}`}>{section.title}</a>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>Loading...</p>
-          )}
+          <div className="sidebar-content">
+            <div className="sidebar-title">
+              <h2>Contents</h2>
+            </div>
+            {sections.length > 0 ? (
+              <ul>
+                {sections.map((section, index) => (
+                  <li key={index}>
+                    <a href={`#${section.title}`}>{section.title}</a>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
         </div>
         <div className="main-content">
-          <h1>{capitalizeString(topic)}</h1>
+          <div className="main-content-title">
+            <h1>{capitalizeString(topic)}</h1>
+          </div>
           <p dangerouslySetInnerHTML={{ __html: addBulletPoints(modifyATags(summary)) }} />
 
           {sections.length > 0 ? (
             <div className="accordion accordion-flush">
               {sections.map((section, index) => (
-                <AccordionItem key={index} topic={topic} section_prop={section} index={index} />
+                <div key={index} ref={(ref) => (sectionRefs.current[index] = ref)}>
+                  <AccordionItem topic={topic} section_prop={section} index={index} />
+                </div>
               ))}
             </div>
           ) : (
